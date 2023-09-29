a0057747893a8eae67595dcb40a3a4682aba1cfa
Keegan Lenz <Keegan.lenz@student.cart.org>
2023-09-29 14:34:10 -0700

    SCSS setup

--- club-website/src/components/nav_bar.tsx
+++ club-website/src/components/nav_bar.tsx
@@ -1,9 +1,11 @@
 function Navigation_Bar() {
-    return <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-    </ul>;
+    return <nav className="nav-bar">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>;
 }
 
 export default Navigation_Bar;
--- club-website/src/index.tsx
+++ club-website/src/index.tsx
@@ -2,9 +2,9 @@ import Navigation_Bar from './components/nav_bar';
 import Club_Header from './components/club_header';
 
 function App() {
-  return <div>
-    <Navigation_Bar></Navigation_Bar>
-    <Club_Header></Club_Header>
+  return <div className="page-wrapper">
+      <Navigation_Bar></Navigation_Bar>
+      <Club_Header></Club_Header>
     </div>
 }
 
