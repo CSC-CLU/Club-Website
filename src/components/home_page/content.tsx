5cdcb50b2ed526aba721569d511e1b9bbe1c0b27
Keegan Lenz <Keegan.lenz@student.cart.org>
2023-10-05 19:00:28 -0700

    'public' folder for images in dist

--- src/components/home_page/content.tsx
+++ src/components/home_page/content.tsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import Projects_Content from "../../content/projects_content";
+import Projects_Content from "../../../content/projects_content";
 
 function Content() {
     let content_list: any[] = [];
